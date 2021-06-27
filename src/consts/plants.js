ea7d8b788c8c3893d6ecc105898e73be48080535
hamzaihsa15 <71670698+hamzaihsa15@users.noreply.github.com>
2021-06-27 12:33:18 +0500

    one

---
+++ src/consts/plants.js
@@ -0,0 +1,44 @@
+const plants = [
+  {
+    id: 1,
+    name: 'Rectangle table',
+    price: '99.99',
+    like: true,
+    img: require('../assets/table1.jpg'),
+    about:
+      'A table is an arrangement of data in rows and columns, or possibly in a more complex structure. Tables are widely used in communication, research, and data analysis. Tables appear in print media, handwritten notes, computer software, architectural ornamentation, traffic signs, and many other places.',
+  },
+
+  {
+    id: 2,
+    name: 'Block Design table',
+    price: '149.99',
+    like: false,
+    img: require('../assets/table2.jpg'),
+    about:
+      'Material: Kikar + MDF, Color: Can be change as per your requirements, Delivery Charges Apply, Dimensions:, Center Table:, Height: 18″, Width:45.',
+  },
+  {
+    id: 3,
+    name: 'Wooden chair',
+    price: '59.99',
+    like: false,
+    img: require('../assets/chair1.jpg'),
+    about:
+      'Wooden chair one of the most popular and beautiful chair that will used for sitting.It is made of finest wood that is used all around the world.',
+  },
+
+  {
+    id: 4,
+    name: 'Pine bed',
+    price: '400.00',
+    like: true,
+    img: require('../assets/bed1.jpg'),
+    about:
+      'Wooden bed one of the most popular and beautiful bed that will used for sleeping.It is made of finest wood that is used all around the world.Economical bed with tilted and padded headboard, solid wood legs. Modern and simple design with a combination of wood textured lamination.',
+  },
+  
+  
+];
+
+export default plants;
